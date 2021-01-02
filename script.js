1e316504f3000ae43e40d78fc62a6248ea581b2d
Martin <matovu.synergy@gmail.com>
2021-01-02 18:13:36 +0300

    Add script.js and include post-cards update (divider and ellipsis) for blog section (home page)

---
+++ script.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function( $ ){
+  /* Home page
+   ========================================================================== */
+   
+  // create title/content divider within post cards
+  $(".post--grid .elementor-posts .elementor-post__card .elementor-post__text > .elementor-post__title").each(function() {
+    $(this).after(
+      '<div class="custom-divider"><span class="post--title-divider"><span></div>'
+    );
+  });
+
+  // append ellipsis within to post content
+  $(".post--grid .elementor-posts .elementor-post__card .elementor-post__excerpt > p").each(function() {
+    $(this).replaceWith($(this).html(function() {
+      var text = $(this).text().slice(0,-1);
+       
+      return text + "...";
+    }));
+  });
+      
+});
